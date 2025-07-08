05d24e660a0ed26fead9e7393551fb8e43f7a7af
Alli Salamone <agsthk@colostate.edu>
2025-07-08 14:42:19 -0600

    Created function to structure Thermo NOx data

--- ResearchInstruments_Scripts/structure_rawdata.py
+++ ResearchInstruments_Scripts/structure_rawdata.py
@@ -7,6 +7,7 @@ Created on Tue Jul  8 10:14:54 2025
 
 import os
 import polars as pl
+import pandas as pd
 
 # Declares full path to ResearchInstruments_Data/ directory
 data_dir = os.getcwd()
@@ -144,10 +145,84 @@ def structure_2btech(raw_dir, struct_dir, inst, source):
         # Writes structured data to CSV file
         data.write_csv(struct_path)
 
-
-for inst in ["2BTech_202", "2BTech_205_A","2BTech_205_B", "2BTech_405nm"]:
-    for source in ["Logger", "SD"]:
-        try:
-            structure_2btech(RAW_DATA_DIR, STRUCT_DATA_DIR, inst, source)
-        except FileNotFoundError:
+def structure_thermo(raw_dir, struct_dir):
+    
+    # Path to directory containing raw ThermoScientific 42i-TL data
+    raw_dir = os.path.join(raw_dir,
+                           "ThermoScientific_42i-TL_RawData",
+                           "ThermoScientific_42i-TL_RawLoggerData")
+    # Path to directory containing corresponding structured data
+    struct_dir = os.path.join(struct_dir,
+                           "ThermoScientific_42i-TL_StructuredData",
+                           "ThermoScientific_42i-TL_StructuredLoggerData")
+    if not os.path.exists(struct_dir):
+        os.makedirs(struct_dir)
+    else:
+        # PLAN: Create a text file that lists the files that have already been structured and read it in here
+        pass
+    
+    columns = {"Flags": "flags",
+               "no": "no_ppb",
+               "no2": "no2_ppb",
+               "nox": "nox_ppb",
+               "pres": "press_mmHg",
+               "intt": "internal_temp_C",
+               "rctt": "chamber_temp_C",
+               "smplf": "sample_flow_LPM",
+               "ozonf": "o3_flow_LPM",
+               "pmtv": "pmt_voltage_V"}
+    
+    for file in os.listdir(raw_dir):
+        raw_path = os.path.join(raw_dir, file)
+        data = pd.read_csv(raw_path, delimiter="\s+", skiprows=5)
+        data = pl.from_pandas(data)
+        if data.is_empty():
             continue
+        # Defines NO2
+        data = data.lazy().with_columns(
+            no2=pl.col("nox").sub(pl.col("no"))
+            )
+        # Defines instrument datetime
+        data = data.select(
+            pl.concat_str(
+                [pl.col("Date"), pl.col("Time").str.strip_chars()],
+                separator=" "
+                ).str.to_datetime(
+                    "%m-%d-%y %H:%M",
+                    strict=False
+                    ).dt.replace_time_zone(
+                        time_zone="MST"
+                   ).alias("mst_datetime"),
+           pl.exclude("Date", "Time", "hino", "hinox", "pmtt", "convt")
+           )
+        # Defines UTC and local datetime
+        data = data.select(
+           pl.col("mst_datetime").dt.convert_time_zone(
+               "UTC"
+               ).alias("utc_datetime"),
+           pl.col("mst_datetime").dt.convert_time_zone(
+               "America/Denver"
+               ).alias("local_datetime"),
+           *columns.keys()
+           ).rename(columns).collect()
+        
+        local_start = data["local_datetime"].min().strftime("%Y%m%d_%H%M%S")
+        local_stop = data["local_datetime"].max().strftime("%Y%m%d_%H%M%S")
+        
+        struct_file_name = ("ThermoScientific_42i-TL_StructuredLoggerData_"
+                            + local_start 
+                            + "_" 
+                            + local_stop 
+                            + ".csv")
+        struct_path = os.path.join(struct_dir, struct_file_name)
+        # Writes structured data to CSV file
+        data.write_csv(struct_path)
+
+# for inst in ["2BTech_202", "2BTech_205_A","2BTech_205_B", "2BTech_405nm"]:
+#     for source in ["Logger", "SD"]:
+#         try:
+#             structure_2btech(RAW_DATA_DIR, STRUCT_DATA_DIR, inst, source)
+#         except FileNotFoundError:
+#             continue
+
+structure_thermo(RAW_DATA_DIR, STRUCT_DATA_DIR)
